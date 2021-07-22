c96ba36cbd62a58e8b88e7e53c097a68d81def31
Shruti <shruti.p2003@gmail.com>
2021-07-22 16:46:01 +0000

    Read data from database!

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,8 +1,14 @@
-
-
 //define the buttons function
 const getMessages = () => {
     //get a value of the text box
     const passcode = document.querySelector("#passcode")
-    console.log("Function runs!", passcode.value)
+    //reference to database in firebase
+    const messagesRef = firebase.database().ref();
+    //event listener that retrieves the data
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val()
+        console.log(data)
+    })
+
+
 }
